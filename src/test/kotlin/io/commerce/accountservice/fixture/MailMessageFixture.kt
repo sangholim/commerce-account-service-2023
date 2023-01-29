96cd1d25225471efd33a580cb357f541960bc683
hoya <hybriddevslim@gmail.com>
2023-01-29 12:36:11 +0900

    test: MailMessageFixture.kt 제목 수정

--- src/test/kotlin/io/commerce/accountservice/fixture/MailMessageFixture.kt
+++ src/test/kotlin/io/commerce/accountservice/fixture/MailMessageFixture.kt
@@ -2,5 +2,5 @@ package io.commerce.accountservice.fixture
 
 object MailMessageFixture {
     const val FROM: String = "contact@commerce.co.kr"
-    const val VERIFICATION_SUBJECT: String = "commerce 이메일 인증하기"
+    const val VERIFICATION_SUBJECT: String = "커머스 이메일 인증하기"
 }
